402551587a50279d2f7320e0c2182a28deee38dd
psychoticvenom <x231231x@gmail.com>
2020-03-25 22:35:18 +0800

    new project

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+// import background from './assets/background.jpg';
+export default {
+  name: 'app',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  width: 100vw;
+  height: 100vh;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background-image: url('./assets/background.jpg');
+  background-size: 100% 100%;
+}
+</style>
