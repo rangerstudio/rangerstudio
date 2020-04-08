7cd29e13d21825e255a4026d2d074c5dbeccc3f4
c024 <c024@oa.fenri.com.tw>
2020-04-08 17:43:56 +0800

    write footer bar icon, animate

--- src/components/footerbar.vue
+++ src/components/footerbar.vue
@@ -1,32 +1,68 @@
 <template>
   <footer class="footer footer__bar">
-    <img
-      class="footer__icon"
-      title="木目金心 Facebook"
-      :src="facebook"
-    />
-    <img
-      class="footer__icon"
-      title="木目金心 Instagram"
-      :src="instagram"
-    />
-    <strong>關於木目金心</strong>
+    <div class="footer footer__bar__external">
+      <a
+        :key="index"
+        v-for="(website, index) in websites"
+        :href="website.link"
+        :title="website.title"
+        target="_blank"
+        @mouseenter="addAnimation(website)"
+        @mouseleave="removeAnimation(website)">
+        <svg
+         :class="`footer__icon animated ${website.animate}`"
+         viewBox="0 0 24 24">
+         <path :d="website.path" />
+        </svg>  
+      </a>
+    </div>
+    <div class="footer footer__bar__internal">
+      <strong
+        :key="index"
+        v-for="(menu, index) in menus">
+        <span class="footer__bar__text">{{ `&nbsp;&nbsp;${menu}&nbsp;&nbsp;` }}</span>  
+        <span v-if="index !== menus.length-1">{{ '|&nbsp;' }}</span> 
+      </strong>
+    </div>
+    <div class="footer footer__bar__copyright">
+      <strong>Copyright © Ranger Studio Inc. All rights reserved</strong>
+    </div>
+    <!-- <strong>關於木目金心</strong>
     <strong>最新消息</strong>
     <strong>作品集</strong>
-    <strong>聯絡我們</strong>
+    <strong>聯絡我們</strong> -->
   </footer>
 </template>
 
 <script>
-import facebook from '../assets/facebook.png';
-import instagram from '../assets/instagram.png';
 export default {
   data() {
     return {
-      facebook,
-      instagram
+      menus: ['關於木目金心', '最新消息', '作品集', '聯絡我們'],
+      websites: [
+        {
+          animate: "",
+          link: "https://www.facebook.com/RangerMakerStudio",
+          title: "木目金心 Facebook",
+          path: "M22.675 0h-21.35c-.732 0-1.325.593-1.325 1.325v21.351c0 .731.593 1.324 1.325 1.324h11.495v-9.294h-3.128v-3.622h3.128v-2.671c0-3.1 1.893-4.788 4.659-4.788 1.325 0 2.463.099 2.795.143v3.24l-1.918.001c-1.504 0-1.795.715-1.795 1.763v2.313h3.587l-.467 3.622h-3.12v9.293h6.116c.73 0 1.323-.593 1.323-1.325v-21.35c0-.732-.593-1.325-1.325-1.325z"
+        },
+        {
+          animate: "",
+          link: "https://www.instagram.com/RangerMakerStudio",
+          title: "木目金心 Instagram",
+          path: "M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"
+        }
+      ]
     }
-  }
+  },
+  methods: {
+    addAnimation(obj) {
+      obj.animate = "rubberBand";
+    },
+    removeAnimation(obj) {
+      obj.animate = "";
+    }
+  },
 }
 </script>
 
@@ -35,11 +71,32 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-wrap: wrap;
   &__bar {
     width: 100vw;
-    height: 15vh;
+    height: 20vh;
+    &__copyright {
+      width: 90%;
+      height: 25%;
+    }
+    &__external {
+      width: 90%;
+      height: 25%;
+    }
+    &__internal {
+      width: 90%;
+      height: 40%;
+      font-size: 1.5rem;
+    }
+    &__text {
+      cursor: pointer;
+      &:hover {
+        font-size: 2rem;
+      }
+    }
   }
   &__icon {
+    margin: 0 1rem;
     width: 2rem;
     height: 2rem;
   }
